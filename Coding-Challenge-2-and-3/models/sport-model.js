a4536c82c88811f15ceee323eb76f0ebf3df201d
Julia Margarita Jimenez Herrera <maggie.jh99@gmail.com>
2020-05-11 08:26:12 -0500

    2 & 3 Challenge

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,7 +1,36 @@
 const mongoose = require( 'mongoose' );
 
-/* Your code goes here */
+
+const sportSchema = mongoose.Schema({
+    id : {
+        type : Number,
+        require : true
+    },
+    name : {
+        type : String,
+        require : true
+    },
+    num_players : {
+        type : Number,
+        require : true
+    }
+});
+
+const sportsCollection = mongoose.model( 'sports', sportSchema);
+
+const Sports = {
+    createSport : function( newSport ){
+        return sportsCollection
+        .create( newSport )
+        .then( createdSport => {
+            return createdSport;
+        })
+        .catch( err => {
+            return err;
+        })
+    }
+}
 
 module.exports = {
-    
+    Sports
 };
--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -3,11 +3,42 @@ const bodyParser = require( 'body-parser' );
 const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
+const { Sports } = require('./models/sport-model');
 
 const app = express();
 
 
-/* Your code goes here */
+app.post( '/sports/addSport/:sportId', jsonParser, (req, res) => {
+    
+    let name = req.body.name;
+    let num_players = req.body.num_players;
+    let id = req.body.id;
+    let sportId = req.params.sportId;
+
+
+    if(!name || !num_players || !id){
+        res.statusMessage = "Please send all the fields";
+        return res.status( 406 ).end();
+    }
+
+    if( sportId !== id ){
+        res.statusMessage = "The id sent and the sportId don't match";
+        return res.status( 409 ).end();
+    }
+
+    const newSport = { id, name, num_players };
+
+    Sports
+        .createSport( newSport )
+        .then( result => {
+            return res.status( 201 ).json( result );
+        })
+        .catch( err => {
+            res.statusMessage = "Something went wrong with the DB";
+            return res.status( 500 ).end();
+        })
+
+})
 
 
 app.listen( PORT, () => {
